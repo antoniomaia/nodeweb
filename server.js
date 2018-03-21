f2f42f353ef34b5c06d3048d5fa11c6f311e05b4
am <antoniobcmaia@gmail.com>
2018-03-21 17:13:43 +0000

    Extract species from multiple pages; Delete node_modules

--- server.js
+++ server.js
@@ -1,49 +1,54 @@
-let express = require('express');
-let request = require('request');
-let cheerio = require('cheerio');
-let fs = require('fs');
-let app = express();
+const express = require('express');
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+const fse = require('fs-extra');
+const app = express();
 
-app.get('/scrape', function (req, res) {
-  // the URL we will scrape from
-  url = 'https://www.worldwildlife.org/species/directory?direction=desc&sort=extinction_status';
+app.get('/species', function (req, res) {
+  // the URLs we will extract data from
+  urls = [
+    'https://www.worldwildlife.org/species/directory?direction=desc&sort=extinction_status',
+    'https://www.worldwildlife.org/species/directory?direction=desc&page=2&sort=extinction_status'
+  ];
 
-  // request call
-  request(url, function (error, response, html) {
-    // check to make sure no errors
-    if (!error) {
-      // utilize the cheerio library to give us jQuery functionality
-      const $ = cheerio.load(html);
-      // variables to capture
-      let commonName, scientificName, conservationStatus;
-      var species = [];
+  let result = {
+    species: []
+  };
+  function parse(html) {
+    let commonName, scientificName, conservationStatus;
 
-      $('table tbody tr').each(function (i, elem) {
-        // store data into a variable so we can easily see what's going on
-        let dataList = $(this).children();
+    const $ = cheerio.load(html);
 
-        if (dataList != null) {
-          species[i] = {
-            commonName: (dataList[0].children[0].firstChild) ? dataList[0].children[0].firstChild.nodeValue : "",
-            scientificName: (dataList[1].children[0].firstChild) ? dataList[1].children[0].firstChild.nodeValue : "",
-            conservationStatus: (dataList[2].children[0]) ? dataList[2].children[0].nodeValue : ""
-          }
-        }
+    $('table tbody tr').each(function (i, elem) {
+      // store data into a variable so we can easily see what's going on
+      let dataList = $(this).children();
 
-      });
+      if (dataList != null) {
+        result.species.push({
+          commonName: (dataList[0].children[0].firstChild) ? dataList[0].children[0].firstChild.nodeValue : "",
+          scientificName: (dataList[1].children[0].firstChild) ? dataList[1].children[0].firstChild.nodeValue : "",
+          conservationStatus: (dataList[2].children[0]) ? dataList[2].children[0].nodeValue : ""
+        });
+      }
 
-    }
-
-    // built in 'fs' library
-    fs.writeFile('output.json', JSON.stringify(species, null, 4), function (err) {
-      console.log('File succcessfully written! - Check your project directory');
     });
 
-    // just send out a message to the browser
-    res.send(species);
-  });
+    return result;
+  }
 
-});
+  let append = file => result => fse.outputFile(file, JSON.stringify(result, null, 4))
+
+  urls.forEach(url => {
+    rp(url)
+      .then(parse)
+      .then(append('output.json'))
+      .then(() => console.log('Success'))
+      .catch(err => console.log('Error: ', err));
+  })
+
+  res.send('Check your console!')
+
+})
 
 app.listen('8081');
 
